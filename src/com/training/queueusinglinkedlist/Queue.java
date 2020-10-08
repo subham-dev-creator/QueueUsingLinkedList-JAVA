863dff7756d5fdaead403c98cdeab74b9d076af5
subhamstark <subham@sap.com>
2020-10-08 16:57:33 +0530

    UC3 Done

--- src/com/training/queueusinglinkedlist/Queue.java
+++ src/com/training/queueusinglinkedlist/Queue.java
@@ -3,8 +3,23 @@ package com.training.queueusinglinkedlist;
 import com.training.queueusinglinkedlist.LinkedList;
 
 public class Queue<T> {
-    private LinkedList<T> Top = new LinkedList<>();
+    private LinkedList<T> queue = new LinkedList<>();
 
+    public void enqueue(T value){
+        queue.addNodeLast(value);
+    }
+
+    public void display(){
+        queue.printList();
+    }
+
+    public static void main(String[] args) {
+        Queue<Integer> queue = new Queue<>();
+        queue.enqueue(56);
+        queue.enqueue(30);
+        queue.enqueue(70);
+        queue.display();
+    }
 
 
 }
