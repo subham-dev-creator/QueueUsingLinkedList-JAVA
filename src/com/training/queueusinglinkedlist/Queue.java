b1d3d9475486688197ab70c6a3bbb05ec8e42efe
subhamstark <subham@sap.com>
2020-10-08 17:00:13 +0530

    UC4 Done

--- src/com/training/queueusinglinkedlist/Queue.java
+++ src/com/training/queueusinglinkedlist/Queue.java
@@ -5,12 +5,19 @@ import com.training.queueusinglinkedlist.LinkedList;
 public class Queue<T> {
     private LinkedList<T> queue = new LinkedList<>();
 
+    // Add Value in queue
     public void enqueue(T value){
         queue.addNodeLast(value);
     }
 
+    // Print Queue
     public void display(){
         queue.printList();
+    }
+
+    // Remove element from queue
+    public void dequeue(){
+        queue.deleteNodeFront();
     }
 
     public static void main(String[] args) {
@@ -18,6 +25,8 @@ public class Queue<T> {
         queue.enqueue(56);
         queue.enqueue(30);
         queue.enqueue(70);
+        queue.display();
+        queue.dequeue();
         queue.display();
     }
 
